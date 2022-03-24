446eddf7b4fc562bbc7c5d9d7d408f2da374c04c
kn <shortpanda34@gmail.com>
2022-03-24 14:20:33 +0900

    fix toppage

--- 2/assets/js/main.js
+++ 2/assets/js/main.js
@@ -1,11 +1,41 @@
 /* globals $*/
 
+$(window.onresize = (function () {
+  let vw = document.body.clientWidth;
+  if (vw > 767) {
+    $(window.onscroll = (function () {
+      let y = window.pageYOffset;
+      if (y > 752) {
+        $('header').addClass('sc-color');;
+      } else {
+        $('header').removeClass('sc-color');
+      }
+    }))
+  } else {
+    $(window.onscroll = (function () {
+      let y = window.pageYOffset;
+      if (y > 599) {
+        $('header').addClass('sc-color');
+      } else {
+        $('header').removeClass('sc-color');
+      }
+    }))
+  }
+}));
 //hamburger
 $('#hamburger-menu').hide();
 $('.fa-times').hide();
 $('#hamburger-button').click(function () {
-  $(this).toggleClass('selected');
   var content = $(this).parents().find('#hamburger-menu');
   content.slideToggle();
   $('.hamburger-bars, .fa-times').toggle();
+  $('header').toggleClass('tg-color');
+  $('body').toggleClass('noscroll');
+});
+
+$('#fv-slick').slick({
+  autoplay: true,
+  autoplaySpeed: 3500,
+  dots: false,
+  arrows: false,
 });
