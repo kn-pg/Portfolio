a78d53323c50ef12a6facf968d39e3b9ae220467
kn <shortpanda34@gmail.com>
2022-04-04 12:53:18 +0900

    new folder setup

---
+++ 3/assets/js/main.js
@@ -0,0 +1,11 @@
+/* globals $*/
+
+//hamburger
+$('#hamburger-menu').hide();
+$('#hamburger-button-hide').hide();
+$('#hamburger-button').click(function () {
+  $(this).toggleClass('selected');
+  var content = $(this).parents().find('#hamburger-menu');
+  content.slideToggle();
+  $('.hamburger-bars, #hamburger-button-hide').toggle();
+});
