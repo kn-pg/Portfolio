36a721ee1dc9f13deaf13ca523aa37645ffbb6db
kn <shortpanda34@gmail.com>
2022-04-08 15:19:08 +0900

    home-tablet&sp fix2

--- 3/assets/js/main.js
+++ 3/assets/js/main.js
@@ -7,5 +7,20 @@ $('#hamburger-button').click(function () {
   $(this).toggleClass('selected');
   var content = $(this).parents().find('#hamburger-menu');
   content.slideToggle();
-  $('.hamburger-bars, .hamburger-button-icon').toggle();
+  $('.hamburger-bars, .hamburger-button-icon .tg-color').toggle();
+});
+//off-page link
+$('.off-page').click(function () {
+  let href = $(this).children('a').attr('href');
+  location.href = href;
+  //window.open(href);
+});
+//in-page link
+$('.in-page').click(function () {
+  var speed = 500; // スクロール速度(ミリ秒)
+  let href = $(this).children('a').attr('href');
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $('html').animate({ scrollTop: position }, speed, 'swing');
+  return false;
 });
